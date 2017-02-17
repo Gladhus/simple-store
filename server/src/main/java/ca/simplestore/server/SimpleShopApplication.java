e3f3f56ebbf8cf17773c27f1b93eeed9a9e82b91
Charles-Antoine Chartrand <chartrand@Charles-Antoines-MacBook-Pro.local>
2017-02-17 08:52:18 -0500

    Refactor

---
+++ server/src/main/java/ca/simplestore/server/SimpleShopApplication.java
@@ -0,0 +1,12 @@
+package ca.simplestore.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleShopApplication.class, args);
+	}
+}
